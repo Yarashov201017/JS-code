d06058e3da3bdbf69b072c38a55fb9d331f59a37
Ulug'bek <ulugbeek256@gmail.com>
2026-04-14 15:34:18 +0500

    Refactor js.js to include user input greeting
    Comment out existing code and add user input functionality.

--- js.js
+++ js.js
@@ -1,5 +1,8 @@
-let res="Hello World".toUpperCase()
-console.log(res)
-function salom() {
-    alert("Salom, dunyo!");
-  }
+// let res="Hello World".toUpperCase()
+// console.log(res)
+// function salom() {
+//     alert("Salom, dunyo!");
+//   }
+let ism=promt('ism kiriting=') //foydalanuvchidan so'z olish
+alert(`salom ${ism}`) //olingan so'zga javob berish
+consol.log(ism) //bu foydalanuvchidan olingan so'zni chiqaradi
